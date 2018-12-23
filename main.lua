72874de7d256bc7038aaedeb321d2b8588ff6eee
Rahul Kumar <rahul@thoughtnirvana.com>
2018-12-24 00:25:42 +0530

    Refactored game objects

--- main.lua
+++ main.lua
@@ -5,6 +5,16 @@ ball.radius = 10
 ball.speed_x = 300
 ball.speed_y = 300
 
+function ball.draw()
+    local segments_in_circle = 16
+    love.graphics.circle('line', ball.x, ball.y, ball.radius, segments_in_circle)
+end
+
+function ball.update(dt)
+    ball.x = ball.x + ball.speed_x * dt
+    ball.y = ball.y + ball.speed_y * dt
+end
+
 
 local platform = {}
 platform.x = 500
@@ -13,23 +23,11 @@ platform.width = 70
 platform.height = 20
 platform.speed_x = 300
 
-
-local brick = {}
-brick.x = 100
-brick.y = 100
-brick.width = 50
-brick.height = 30
-
-
-function love.load()
-
+function platform.draw()
+    love.graphics.rectangle('line', platform.x, platform.y, platform.width, platform.height)
 end
 
-
-function love.update(dt)
-    ball.x = ball.x + ball.speed * dt
-    ball.y = ball.y + ball.speed * dt
-    
+function platform.update(dt)
     if love.keyboard.isDown('right') then
         platform.x = platform.x + platform.speed_x * dt
     end
@@ -40,11 +38,70 @@ function love.update(dt)
 end
 
 
-function love.draw()
-    local segments_in_circle = 16
-    love.graphics.circle('line', ball.x, ball.y, ball.radius, segments_in_circle)
-    love.graphics.rectangle('line', platform.x, platform.y, platform.width, platform.height)
+local bricks = {}
+
+bricks.current_level_bricks = {}
+bricks.width = 50
+bricks.height = 30
+
+function bricks.new_brick(x, y, width, height)
+    return ({
+        x = x,
+        y = y,
+        width = width or bricks.width,
+        height = height or bricks.height
+    })
+end
+
+function bricks.draw_brick(brick)
     love.graphics.rectangle('line', brick.x, brick.y, brick.width, brick.height)
+end
+
+function bricks.update_brick(brick)
+
+end
+
+function bricks.add_to_current_level(brick)
+    table.insert(bricks.current_level_bricks, brick)
+end
+
+function bricks.draw()
+    for _, brick in pairs(bricks.current_level_bricks) do
+        bricks.draw_brick(brick)
+    end
+end
+
+function bricks.update(dt)
+    for _, brick in pairs(bricks.current_level_bricks) do
+        bricks.update_brick(brick)
+    end
+end
+
+
+function love.load()
+    bricks.add_to_current_level(bricks.new_brick( 100, 100))
+    bricks.add_to_current_level(bricks.new_brick( 160, 100))
+end
+
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.push('quit')
+    end
+end
+
+
+function love.update(dt)
+    ball.update(dt)
+    platform.update(dt)
+    bricks.update(dt)
+end
+
+
+function love.draw()
+    ball.draw()
+    platform.draw()
+    bricks.draw()
 end
 
 
