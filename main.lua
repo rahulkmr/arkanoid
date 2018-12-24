eb000b64794a6dd3ab70340e28550e11914ed33b
Rahul Kumar <rahul@thoughtnirvana.com>
2018-12-24 10:57:07 +0530

    collision shift values

--- main.lua
+++ main.lua
@@ -183,11 +183,22 @@ end
 
 function collisions.check_rectangle_overlap(a, b)
     local overlap = false
-   if not( a.x + a.width < b.x  or b.x + b.width < a.x  or
-           a.y + a.height < b.y or b.y + b.height < a.y ) then
-      overlap = true
-   end
-   return overlap
+    local shift_b_x, shift_b_y = 0, 0
+    if not( a.x + a.width < b.x  or b.x + b.width < a.x  or
+            a.y + a.height < b.y or b.y + b.height < a.y ) then
+        overlap = true
+        if (a.x + a.width / 2) < (b.x + b.width / 2) then
+            shift_b_x = (a.x + a.width) - b.x
+        else
+            shift_b_x = a.x - (b.x + b.width)
+        end
+        if (a.y + a.height / 2) < (b.y + b.height / 2) then
+            shift_b_y = (a.y + a.height) - b.y
+        else
+            shift_b_y = a.y - (b.y + b.height)
+        end
+    end
+    return overlap, shift_b_x, shift_b_y
 end
 
 function collisions.ball_platform_collision(ball, platform)
