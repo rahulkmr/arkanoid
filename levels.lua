56c5986618905830332e9bfb01b6c648ea9ea80e
Rahul Kumar <rahul@thoughtnirvana.com>
2018-12-24 12:32:42 +0530

    Simple gamestate

--- levels.lua
+++ levels.lua
@@ -4,20 +4,6 @@ levels.current_level = 1
 levels.game_finished = false
 levels.sequence = require 'levels/sequence'
 
-function levels.switch_to_next_level(bricks, ball)
-    if #bricks.current_level_bricks == 0 then
-        if levels.current_level < #levels.sequence then
-            levels.current_level = levels.current_level + 1
-            level = levels.require_current_level()
-            bricks.construct_level(level)
-            ball.reposition()
-        else
-            levels.game_finished = true
-        end
-    end
-end
-
-
 function levels.require_current_level()
     local level_filename = 'levels/' .. levels.sequence[levels.current_level]
     local level = require(level_filename)
--- main.lua
+++ main.lua
@@ -5,6 +5,7 @@ local walls = require 'walls'
 local collisions = require 'collisions'
 local levels = require 'levels'
 
+local gamestate = 'menu'
 
 function love.load()
     walls.construct_walls()
@@ -12,30 +13,82 @@ function love.load()
 end
 
 function love.keypressed(key)
-    if key == 'escape' then
-        love.event.push('quit')
+    if gamestate == 'menu' then
+        if key == 'return' then
+            gamestate = 'game'
+        elseif key == 'escape' then
+            love.event.quit()
+        end
+    elseif gamestate == 'game' then
+        if key == 'escape' then
+            gamestate = 'gamepaused'
+        end
+    elseif gamestate == 'gamepaused' then
+        if key == 'return' then
+            gamestate = 'game'
+        elseif key == 'escape' then
+            love.event.quit()
+        end
+    elseif gamestate == 'gamefinished' then
+        if key == 'return' then
+            levels.current_level = 1
+            level = levels.require_current_level()
+            bricks.construct_level(level)
+            ball.reposition()
+            gamestate = 'game'
+        elseif key == 'escape' then
+            love.event.quit()
+        end
+    end
+end
+
+function switch_to_next_level(bricks, ball)
+    if #bricks.current_level_bricks == 0 then
+        if levels.current_level < #levels.sequence then
+            levels.current_level = levels.current_level + 1
+            level = levels.require_current_level()
+            bricks.construct_level(level)
+            ball.reposition()
+        else
+            gamestate = 'gamefinished'
+        end
     end
 end
 
 
+
 function love.update(dt)
-    ball.update(dt)
-    platform.update(dt)
-    bricks.update(dt)
-    walls.update(dt)
-    collisions.resolve_collisions(ball, platform, walls, bricks)
-    levels.switch_to_next_level(bricks, ball)
+    if gamestate == 'menu' then
+    elseif gamestate == 'game' then
+        ball.update(dt)
+        platform.update(dt)
+        bricks.update(dt)
+        walls.update(dt)
+        collisions.resolve_collisions(ball, platform, walls, bricks)
+        switch_to_next_level(bricks, ball, gamestate)
+    elseif gamestate == 'gamepaused' then
+    elseif gamestate == 'gamefinished' then
+    end
 end
 
 
 function love.draw()
-    ball.draw()
-    platform.draw()
-    bricks.draw()
-    walls.draw()
-    if levels.game_finished then
-        love.graphics.printf('Congratulations!\n' .. 'You have finisehd the game',
-            300, 250, 200, 'center')
+    if gamestate == 'menu' then
+        love.graphics.print('Press Enter to continue', 280, 250)
+    else
+        ball.draw()
+        platform.draw()
+        bricks.draw()
+        walls.draw()
+        if gamestate == 'gamepaused' then
+            love.graphics.print('Game is paused. Press Enter to continue or Esc to quit',
+                50, 50)
+        elseif gamestate == 'gamefinished' then
+            love.graphics.printf('Congratulations!\n' .. 
+            'You have finisehd the game\n' ..
+            'Press Enter to restart or Esc to quit',
+                300, 250, 200, 'center')
+        end
     end
 end
 
